8a742061cfa1f623b11379d561f2799c2366be35
pacholoamit <pacholoamit.tech@gmail.com>
2022-04-12 13:27:05 +0800

    set type manually for user

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,16 +1,21 @@
 import { useAuth } from '@redwoodjs/auth'
+import { SupabaseUser } from '@redwoodjs/auth/dist/authClients'
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 import SignoutBtn from 'src/components/SignoutBtn/SignoutBtn'
 
 const HomePage = () => {
   const { userMetadata } = useAuth()
+
+  const user = userMetadata as SupabaseUser
+  const name = user.user_metadata.full_name ?? user.email
+
   return (
     <>
       <MetaTags title="Home" description="Home page" />
 
       <h1>HomePage</h1>
-      <p>Welcome {JSON.stringify(userMetadata, null, 2)}</p>
+      <p>Welcome {name}</p>
       <SignoutBtn />
       <p>
         Find me in <code>./web/src/pages/HomePage/HomePage.tsx</code>
