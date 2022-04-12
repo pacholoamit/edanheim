b05a92a29c9b9334fcae4639cf5918f9e6147de0
pacholoamit <pacholoamit.tech@gmail.com>
2022-04-12 13:01:27 +0800

    minor modifications to github auth

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,13 +1,16 @@
+import { useAuth } from '@redwoodjs/auth'
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 import SignoutBtn from 'src/components/SignoutBtn/SignoutBtn'
 
 const HomePage = () => {
+  const { userMetadata } = useAuth()
   return (
     <>
       <MetaTags title="Home" description="Home page" />
 
       <h1>HomePage</h1>
+      <p>Welcome {JSON.stringify(userMetadata, null, 2)}</p>
       <SignoutBtn />
       <p>
         Find me in <code>./web/src/pages/HomePage/HomePage.tsx</code>
