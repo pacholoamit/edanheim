36835445cde954845c234be32d4a11a2b6db6130
pacholoamit <pacholoamit.tech@gmail.com>
2022-04-13 22:53:01 +0800

    added Navbar options

--- web/src/layouts/MainLayout/components/AppNavbar/AppNavbar.tsx
+++ web/src/layouts/MainLayout/components/AppNavbar/AppNavbar.tsx
@@ -9,9 +9,73 @@ import {
   Box,
   UnstyledButton,
   useMantineTheme,
+  ThemeIcon,
+  DefaultMantineColor,
 } from '@mantine/core'
+import {
+  ChevronRight,
+  AlertCircle,
+  GitPullRequest,
+  Messages,
+  Database,
+} from 'tabler-icons-react'
 
-import { ChevronRight, ChevronLeft } from 'tabler-icons-react'
+interface NavBarOptionProps {
+  icon: React.ReactNode
+  color: DefaultMantineColor
+  label: string
+}
+
+const NavBarOption: React.FC<NavBarOptionProps> = ({ icon, color, label }) => {
+  return (
+    <UnstyledButton
+      sx={(theme) => ({
+        display: 'block',
+        width: '100%',
+        padding: theme.spacing.xs,
+        borderRadius: theme.radius.sm,
+        color:
+          theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
+
+        '&:hover': {
+          backgroundColor:
+            theme.colorScheme === 'dark'
+              ? theme.colors.dark[6]
+              : theme.colors.gray[0],
+        },
+      })}
+    >
+      <Group>
+        <ThemeIcon color={color} variant="light">
+          {icon}
+        </ThemeIcon>
+
+        <Text size="sm">{label}</Text>
+      </Group>
+    </UnstyledButton>
+  )
+}
+
+const NavBarOptions = () => {
+  const options = [
+    {
+      icon: <GitPullRequest size={16} />,
+      color: 'blue',
+      label: 'Pull Requests',
+    },
+    { icon: <AlertCircle size={16} />, color: 'teal', label: 'Open Issues' },
+    { icon: <Messages size={16} />, color: 'violet', label: 'Discussions' },
+    { icon: <Database size={16} />, color: 'grape', label: 'Databases' },
+  ]
+
+  return (
+    <>
+      {options.map((options) => (
+        <NavBarOption {...options} key={options.label} />
+      ))}
+    </>
+  )
+}
 
 const UserDetails = () => {
   const { user } = useCurrentUser()
@@ -42,29 +106,23 @@ const UserDetails = () => {
     },
   }
   return (
-    <Navbar.Section>
-      <Box sx={sx.box}>
-        <UnstyledButton sx={sx.unstyledButton}>
-          <Group>
-            <Avatar src={user.avatar} radius="xl" />
-            <Box sx={{ flex: 1 }}>
-              <Text size="sm" weight={500}>
-                {user.name}
-              </Text>
-              <Text color="dimmed" size="xs">
-                {user.email}
-              </Text>
-            </Box>
+    <Box sx={sx.box}>
+      <UnstyledButton sx={sx.unstyledButton}>
+        <Group>
+          <Avatar src={user.avatar} radius="xl" />
+          <Box sx={{ flex: 1 }}>
+            <Text size="sm" weight={500}>
+              {user.name}
+            </Text>
+            <Text color="dimmed" size="xs">
+              {user.email}
+            </Text>
+          </Box>
 
-            {theme.dir === 'ltr' ? (
-              <ChevronRight size={18} />
-            ) : (
-              <ChevronLeft size={18} />
-            )}
-          </Group>
-        </UnstyledButton>
-      </Box>
-    </Navbar.Section>
+          <ChevronRight size={18} />
+        </Group>
+      </UnstyledButton>
+    </Box>
   )
 }
 interface AppNavbarProps {
@@ -79,11 +137,13 @@ const AppNavbar: React.FC<AppNavbarProps> = ({ opened }) => {
       hidden={!opened}
       width={{ sm: 200, lg: 300 }}
     >
-      <Navbar.Section>
-        <Text>Section</Text>
+      <Navbar.Section grow>
+        <NavBarOptions />
       </Navbar.Section>
       <Divider />
-      <UserDetails />
+      <Navbar.Section>
+        <UserDetails />
+      </Navbar.Section>
     </Navbar>
   )
 }
