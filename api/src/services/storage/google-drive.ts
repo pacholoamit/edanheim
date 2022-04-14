8dbdc1cfa918286c8699c65adc5fdec1b45bfc88
pacholoamit <pacholoamit.tech@gmail.com>
2022-04-14 22:34:43 +0800

    minor changes

--- api/src/services/storage/google-drive.ts
+++ api/src/services/storage/google-drive.ts
@@ -16,7 +16,11 @@ export const googleDriveFiles = async ({ session }: WebSession) => {
 
   const drive = google.drive({ version: 'v3', auth: oauth2 })
   // TODO: Pagination for Google drive files
-  const { data } = await drive.files.list()
+  const { data } = await drive.files.list({
+    pageSize: 1000,
+    orderBy: 'modifiedTime desc',
+    q: `mimeType != 'application/vnd.google-apps.folder'`,
+  })
 
   return data.files
 }
--- web/src/components/GoogleDriveFilesCell/GoogleDriveFilesCell.tsx
+++ web/src/components/GoogleDriveFilesCell/GoogleDriveFilesCell.tsx
@@ -1,6 +1,6 @@
 import type { googleDriveFiles } from 'types/graphql'
 import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
-import { Card, Grid, Text, Image, Space } from '@mantine/core'
+import { Card, Grid, Text, Image, Space, Tooltip } from '@mantine/core'
 import { edanheimLogo } from 'src/constants'
 
 export const QUERY = gql`
@@ -30,21 +30,23 @@ export const Success = ({
       {googleDriveFiles.map((file) => {
         return (
           <Grid.Col key={file.id} sm={2}>
-            <Card shadow={'lg'} key={file.id} p="lg" sx={{ height: 200 }}>
+            <Card shadow={'lg'} key={file.id} p="md" sx={{ height: 200 }}>
               <Card.Section>
                 <Image src={edanheimLogo} height={160} alt={file.name} />
               </Card.Section>
               <Space h={'sm'} />
-              <Text
-                size="sm"
-                sx={(theme) => ({
-                  color: theme.colors.gray[7],
-                })}
-                weight={'bold'}
-                lineClamp={1}
-              >
-                {file.name}
-              </Text>
+              <Tooltip label={file.name}>
+                <Text
+                  size="sm"
+                  sx={(theme) => ({
+                    color: theme.colors.gray[7],
+                  })}
+                  weight={'bold'}
+                  lineClamp={1}
+                >
+                  {file.name}
+                </Text>
+              </Tooltip>
             </Card>
           </Grid.Col>
         )
