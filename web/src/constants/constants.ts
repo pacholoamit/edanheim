8f75514d6db884373b89723912f23ff97fa1d21c
pacholoamit <pacholoamit.tech@gmail.com>
2022-04-15 15:50:37 +0800

    created basic Storage provider page

--- web/src/constants/constants.ts
+++ web/src/constants/constants.ts
@@ -1,1 +1,4 @@
 export const edanheimLogo = require('public/assets/logo.png')
+export const AwsS3Svg = require('public/assets/providers/aws-s3.svg')
+export const googleDriveSvg = require('public/assets/providers/google-drive.svg')
+export const microsoftOwncloudSvg = require('public/assets/providers/microsoft-owncloud.svg')
--- web/src/pages/StoragePage/StoragePage.tsx
+++ web/src/pages/StoragePage/StoragePage.tsx
@@ -1,19 +1,54 @@
-import { Center, MediaQuery, Title } from '@mantine/core'
+import { Grid, MediaQuery, Stack, Title, Card } from '@mantine/core'
+import AwsS3SVG from 'public/assets/providers/aws.svg'
+import GoogleDriveSVG from 'public/assets/providers/google-drive.svg'
+import MicrosoftOneDriveSVG from 'public/assets/providers/microsoft-onedrive.svg'
 
 const sx = {
   mediaQuery: {
     marginTop: '15rem',
+    marginLeft: '20%',
+    marginRight: '20%',
   },
-  center: {
+  stack: {
     marginTop: '1rem',
+  },
+  cardSection: {
+    height: '100%',
   },
 }
 const StoragePage = () => {
   return (
     <MediaQuery largerThan={'sm'} styles={sx.mediaQuery}>
-      <Center sx={sx.center}>
+      <Stack sx={sx.stack} justify="center" align={'center'}>
         <Title>Add Storage Provider</Title>
-      </Center>
+        <Grid gutter={'xl'}>
+          <Grid.Col md={4} sm={12}>
+            <Card shadow="lg">
+              <Card.Section>
+                <AwsS3SVG style={{ width: '100%', height: '100%' }} />
+              </Card.Section>
+            </Card>
+          </Grid.Col>
+
+          <Grid.Col md={4} sm={12}>
+            <Card shadow="lg">
+              <Card.Section>
+                <GoogleDriveSVG style={{ width: '100%', height: '100%' }} />
+              </Card.Section>
+            </Card>
+          </Grid.Col>
+
+          <Grid.Col md={4} sm={12}>
+            <Card shadow="lg">
+              <Card.Section>
+                <MicrosoftOneDriveSVG
+                  style={{ width: '100%', height: '100%' }}
+                />
+              </Card.Section>
+            </Card>
+          </Grid.Col>
+        </Grid>
+      </Stack>
     </MediaQuery>
   )
 }
