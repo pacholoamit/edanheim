b40956d2ecddba9db515f94ccfd42807f3337d29
pacholoamit <pacholoamit.tech@gmail.com>
2022-04-17 00:24:43 +0800

    remove deprecated graphql query

--- web/src/graphql/queries/FIND_USER.tsx
+++
@@ -1,9 +0,0 @@
-export const FIND_USER = gql`
-  query FindUserQuery($supabaseId: String!) {
-    user: user(supabaseId: $supabaseId) {
-      id
-      email
-      name
-    }
-  }
-`
--- web/src/graphql/queries/index.ts
+++ web/src/graphql/queries/index.ts
@@ -1,2 +1,1 @@
 export * from './GET_GOOGLE_DRIVE_AUTH_URL'
-export * from './FIND_USER'
