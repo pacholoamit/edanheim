ae4394e364452f91d39cd50211d15e29610b82e3
pacholoamit <pacholoamit.tech@gmail.com>
2022-04-18 20:46:20 +0800

    Added graphql caching through response-cache

--- api/src/functions/graphql.ts
+++ api/src/functions/graphql.ts
@@ -1,4 +1,5 @@
 import { createGraphQLHandler } from '@redwoodjs/graphql-server'
+import { useResponseCache } from '@envelop/response-cache'
 
 import directives from 'src/directives/**/*.{js,ts}'
 import sdls from 'src/graphql/**/*.sdl.{js,ts}'
@@ -11,11 +12,16 @@ import { logger } from 'src/lib/logger'
 
 export const handler = createGraphQLHandler({
   getCurrentUser,
-  loggerConfig: { logger, options: {} },
+  loggerConfig: {
+    logger,
+    options: { operationName: true, tracing: true, query: true },
+  },
   directives,
   sdls,
   services,
   defaultError: 'Oops! Something went wrong, sorry about that',
+  extraPlugins: [useResponseCache()],
+
   onException: () => {
     // Disconnect from your database with an unhandled exception.
     db.$disconnect()
